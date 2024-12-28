c049555f8ac1e4cb8f888c08ff05ca5d3599980c
prashant7767 <cms@goldmineltd.com>
2024-12-28 12:51:52 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =744467767474
+let accountEmail="abcd@gmail.com"
+var accountPass ="fhgvjhvkbk15"
+accountcity ="mumbai"
+
+accountEmail="mnop@gmail.com"
+
+// can not use or change const in JS,
+// can use or change value in let.
+
+console.log([accountId,accountEmail])
+
+//console.table([accountId,accountEmail,accountPass,accountcity])- for printinf value in table format
